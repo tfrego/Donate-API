18026189d17dc17ca506b524de14059aba2af6bd
Trang <trang.frego@gmail.com>
2019-01-20 21:10:30 -0800

    add cityState to location class and updated lat and lng to double data type

--- src/main/java/capstone/donate_api/models/Location.java
+++ src/main/java/capstone/donate_api/models/Location.java
@@ -1,17 +1,23 @@
 package capstone.donate_api.models;
 
 public class Location {
-	public String lat;
-	public String lng;
+	public Double lat;
+	public Double lng;
+	public String cityState;
 	
-	public Location(String lat, String lng) {
+	public Location(Double lat, Double lng, String cityState) {
 		this.lat = lat;
 		this.lng = lng;
+		this.cityState = cityState;
 	}
 	
-	public String getLat() { return lat; }
-	public void setLat(String lat) { this.lat = lat; }
+	public Double getLat() { return lat; }
+	public void setLat(Double lat) { this.lat = lat; }
 	
-	public String getLng() { return lng; }
-	public void setLng(String lng) { this.lng = lng; }
+	public Double getLng() { return lng; }
+	public void setLng(Double lng) { this.lng = lng; }
+	
+	public String getCityState() { return cityState; }
+	public void setCityState(String cityState) { this.cityState = cityState; }
+	
 }
